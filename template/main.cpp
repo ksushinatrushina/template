71b22af2a8b776efbf4b712244607abd7b0712b9
ksushinatrushina <132398346+ksushinatrushina@users.noreply.github.com>
2023-07-13 11:18:25 +0200

    template

--- template/main.cpp
+++ template/main.cpp
@@ -67,29 +67,36 @@ auto mult(FirstT first, SecondT second) {
 }
 
 int main() {
-    std::vector<int> c{1, 2, 3, 4};
-    std::vector<int> d{1, 2};
+    std::vector<int> a{1, 2, 3, 4};
+    std::vector<int> b{1, 2};
 
-    std::cout << "Result of subtracting: ";
-    std::vector<int> res = substr(c, d);
+    std::cout << "Result of subtracting №1 (string): ";
+    std::vector<int> res = substr(a, b);
     std::copy(res.begin(), res.end(),
               std::ostream_iterator<int>(std::cout, ", "));
     int res1_2 = substr(1, 2);
 
+    std::cout << std::endl;
+    std::cout << "Result of subtracting №2 (int): " << res1_2 << std::endl;
     std::cout << std::endl << std::endl;
 
-    std::cout << "Result of dividing: ";
-    std::vector<int> res2 = divide(c, d);
+    std::cout << "Result of dividing №1 (string): ";
+    std::vector<int> res2 = divide(a, b);
     std::copy(res2.begin(), res2.end(),
               std::ostream_iterator<int>(std::cout, ", "));
     int res2_2 = divide(1, 2);
 
+    std::cout << std::endl;
+    std::cout << "Result of dividing №2 (int): " << res2_2 << std::endl;
     std::cout << std::endl << std::endl;
 
-    std::cout << "Result of multiplication: ";
-    std::vector<int> res3 = mult(c, d);
+    std::cout << "Result of multiplication №1 (string): ";
+    std::vector<int> res3 = mult(a, b);
     std::copy(res3.begin(), res3.end(),
               std::ostream_iterator<int>(std::cout, ", "));
     int res3_2 = mult(1, 2);
+
+    std::cout << std::endl;
+    std::cout << "Result of multiplication №2 (int): " << res3_2 << std::endl;
 
 }
